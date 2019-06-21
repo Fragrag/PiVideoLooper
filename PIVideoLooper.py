9adb0180d8289c88464a45a973d79c6cc27e3eda
Fragrag <haryo.sukmawanto@gmail.com>
2019-06-21 11:47:50 +0200

    Making project relative path friendly

--- PIVideoLooper.py
+++ PIVideoLooper.py
@@ -1,20 +1,25 @@
 import subprocess
 import json
+from os.path import dirname, join
 
-with open('config.json') as configfile:
+abspath = dirname(__file__)
+
+with open(join(abspath, 'config.json')) as configfile:
+    print("JSON File loaded at " + join(abspath, 'config.json'))
     config = json.load(configfile)
 
 command = ["omxplayer"]
 
 if config["subtitles"] == True:
-    command.append("--subtitles" + config["subtitlesLocation"])
+    command.append("--subtitles" + join(abspath, config["subtitlesLocation"]))
 if config["loop"] == True:
     command.append("--loop")
 if config["noInterface"] == True:
     command.append("--no-osd")
 
-command.append(config['fileLocation'])
+command.append(join(abspath, config['fileLocation']))
 
+print("Running command with parameters:")
 print(command)
 
-omxprocess = subprocess.Popen(command, stdin=subprocess.PIPE)
+#omxprocess = subprocess.Popen(command, stdin=subprocess.PIPE)
